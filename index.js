001cae215ca23e1a82b5ad7a34bc8024ef979ec2
Benji <bwolfenberger@gmail.com>
2021-06-09 15:58:58 -0700

    added .env for hidden api key

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
-
+const axios = require('axios')
 const app = express()
 const PORT = 3000
+require('dotenv').config()
+const financeApiKey = process.env.FMP_API_KEY
 
 app.get('/', (req, res) => {
-    res.send('hi eri')
+    let financeUrl = 'https://financialmodelingprep.com/api/v3/quote/BTCUSD?apikey=' + financeApiKey
+    axios.get(financeUrl)
+    .then(apiRes => {
+        let financeData = apiRes.data
+        res.send(financeData[0].name + financeData[0].price)
+    })
 })
 
 app.listen(PORT, () => {
