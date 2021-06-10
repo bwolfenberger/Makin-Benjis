2720bfb02791effc92fb56c8b52526a154498b1f
Benji <bwolfenberger@gmail.com>
2021-06-10 11:27:33 -0700

    added 30 stocks to show up on main page

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const financeApiKey = process.env.FMP_API_KEY
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    let financeUrl = 'https://financialmodelingprep.com/api/v3/quote/BTCUSD?apikey=' + financeApiKey
+    let financeUrl = `https://financialmodelingprep.com/api/v3/dowjones_constituent?apikey=${financeApiKey}`
     axios.get(financeUrl)
     .then(apiRes => {
         let financeData = apiRes.data
@@ -16,6 +16,15 @@ app.get('/', (req, res) => {
 //        res.send(financeData[0].name + financeData[0].price)
     })
 })
+// app.get('/:id', (req, res) => {
+//     let financeUrl = `https://financialmodelingprep.com/api/v3/quote/${req.params.id}?apikey=${financeApiKey}`
+//     axios.get(financeUrl)
+//     .then(apiRes => {
+//         let financeData = apiRes.data
+//         res.render('index', { financeData})
+// //        res.send(financeData[0].name + financeData[0].price)
+//     })
+// })
 
 app.listen(PORT, () => {
     console.log(`listening to ${PORT} 🐣`)
