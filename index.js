8190bb5237b688c27ad403984595cf113c5ede5f
Benji <bwolfenberger@gmail.com>
2021-06-09 17:02:22 -0700

    added ejs functionality

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const PORT = 3000
 require('dotenv').config()
 const financeApiKey = process.env.FMP_API_KEY
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     let financeUrl = 'https://financialmodelingprep.com/api/v3/quote/BTCUSD?apikey=' + financeApiKey
     axios.get(financeUrl)
     .then(apiRes => {
         let financeData = apiRes.data
-        res.send(financeData[0].name + financeData[0].price)
+        res.render('index', { financeData})
+//        res.send(financeData[0].name + financeData[0].price)
     })
 })
 
